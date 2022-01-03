c599529ed7b3d423e8e850b664dd72ecb0111167
mastercoms <mastercoms@tuta.io>
2022-01-03 06:50:06 -0500

    fix indentation

--- Source/SplashSettings/Private/SplashSettings.cpp
+++ Source/SplashSettings/Private/SplashSettings.cpp
@@ -4,19 +4,19 @@
 
 void FSplashSettingsModule::StartupModule()
 {
-    const FString& SplashDirectory = FPaths::ConvertRelativePathToFull(FPaths::ProjectContentDir() / TEXT("Splash"));
-    TArray<FString> AllSplashFiles;
-    IFileManager::Get().FindFiles(AllSplashFiles, *(SplashDirectory / TEXT("*.png")), true, false);
-    IFileManager::Get().FindFiles(AllSplashFiles, *(SplashDirectory / TEXT("*.jpg")), true, false);
-    IFileManager::Get().FindFiles(AllSplashFiles, *(SplashDirectory / TEXT("*.bmp")), true, false);
+	const FString& SplashDirectory = FPaths::ConvertRelativePathToFull(FPaths::ProjectContentDir() / TEXT("Splash"));
+	TArray<FString> AllSplashFiles;
+	IFileManager::Get().FindFiles(AllSplashFiles, *(SplashDirectory / TEXT("*.png")), true, false);
+	IFileManager::Get().FindFiles(AllSplashFiles, *(SplashDirectory / TEXT("*.jpg")), true, false);
+	IFileManager::Get().FindFiles(AllSplashFiles, *(SplashDirectory / TEXT("*.bmp")), true, false);
 
 	const TArray<FString>& SplashFiles = AllSplashFiles.FilterByPredicate([&](const FString& File) {
 #if WITH_EDITOR
-        return File.StartsWith(TEXT("EdSplash"));
+		return File.StartsWith(TEXT("EdSplash"));
 #else
 		return File.StartsWith(TEXT("Splash"));
 #endif
-    });
+	});
 
 	const TArray<FString>::SizeType& Num = SplashFiles.Num();
 
